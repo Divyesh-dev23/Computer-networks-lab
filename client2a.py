aa52d2b5288611f374934e1e2e36f225ea49c89f
Divyesh Mahapatruni <divyeshmahapatruni@gmail.com>
2026-09-01 14:23:09 +0000

    Add TCP file transfer client and server

--- client2a.py
+++ client2a.py
@@ -3,9 +3,9 @@ import socket
 HOST = "127.0.0.1"
 PORT = 5056
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-client_socket.connect((HOST, PORT))
+client_socket.connect((HOST,PORT))
 
 filename = input("Enter file name: ")
 
@@ -13,7 +13,7 @@ client_socket.send(filename.encode())
 
 data = client_socket.recv(4096).decode()
 
-print("\nFile Contents:\n")
+print("File content:\n")
 print(data)
 
 client_socket.close()
--- server2a.py
+++ server2a.py
@@ -3,28 +3,27 @@ import socket
 HOST = "127.0.0.1"
 PORT = 5056
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-server_socket.bind((HOST, PORT))
+server_socket.bind((HOST,PORT))
 
 server_socket.listen(5)
 
 print("Server is listening...")
 
 while True:
-    client_socket, address = server_socket.accept()
+    client_socket , address = server_socket.accept()
     print("Connected to:", address)
-
     filename = client_socket.recv(1024).decode()
 
     try:
-        with open(filename, "r") as file:
+        with open(filename , "r") as file:
             data = file.read()
-    except FileNotFoundError:
-        data = "File not found."
 
+    except FileNotFoundError:
+        data = "File not found"
+    
     client_socket.send(data.encode())
 
     client_socket.close()
-
     print("Connection closed.\n")
